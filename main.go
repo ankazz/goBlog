aed27153429eb31b8469a8600937464af5c68e39
ankaz <nurzhan1990@gmail.com>
2019-09-17 22:11:49 +0500

    initial git

---
+++ main.go
@@ -0,0 +1,117 @@
+package main
+
+import (
+	"github.com/codegangsta/martini"
+	"github.com/martini-contrib/render"
+	"github.com/russross/blackfriday"
+
+	"./models"
+	"crypto/rand"
+	"fmt"
+	//"log"
+	"html/template"
+	"net/http"
+)
+
+var posts map[string]*models.Post
+
+func indexHandler(rnd render.Render){
+	rnd.HTML(200, "index", posts)
+}
+
+func writeHandler(rnd render.Render){
+	rnd.HTML(200, "write", nil)
+}
+
+func editHandler(rnd render.Render, r *http.Request, params martini.Params) {
+	id := params["id"]
+	post, found := posts[id]
+	if !found {
+		rnd.Redirect("/")
+		return
+	}
+
+	rnd.HTML(200, "write", post)
+}
+
+func savePostHandler(rnd render.Render, r *http.Request){
+	id := r.FormValue("id")
+	title := r.FormValue("title")
+	contentMarkdown := r.FormValue("content")
+	contentHtml := string(blackfriday.MarkdownBasic([]byte(contentMarkdown)))
+
+	var post *models.Post
+	if id != "" {
+		post = posts[id]
+		post.Title = title
+		post.ContentHtml = contentHtml
+		post.ContentMarkdown = contentMarkdown
+	} else {
+		id = GenerateId()
+		post := models.NewPost(id, title, contentHtml, contentMarkdown)
+		posts[post.Id] = post
+	}
+
+	rnd.Redirect("/")
+}
+
+func GenerateId() string {
+	b := make([]byte, 16)
+	rand.Read(b)
+	return fmt.Sprintf("%x", b)
+}
+
+func deleteHandler(rnd render.Render, r *http.Request, params martini.Params) {
+	id := params["id"]
+	if id == "" {
+		rnd.Redirect("/")
+		return
+	}
+
+	delete(posts, id)
+
+	rnd.Redirect("/")
+}
+
+func getHtmlHandler(rnd render.Render, r *http.Request) {
+	md := r.FormValue("md")
+	htmlBytes := blackfriday.MarkdownBasic([]byte(md))
+
+	rnd.JSON(200, map[string]interface{}{"html": string(htmlBytes)})
+}
+
+func unescape(x string) interface{} {
+	return template.HTML(x)
+}
+
+
+func main()  {
+	fmt.Println("Listining on port :3000")
+	posts = make(map[string]*models.Post, 0)
+
+	m := martini.Classic()
+
+	unescapeFuncMap := template.FuncMap{"unescape": unescape}
+
+	m.Use(render.Renderer(render.Options{
+		Directory:  "templates",                         // Specify what path to load the templates from.
+		Layout:     "layout",                            // Specify a layout template. Layouts can call {{ yield }} to render the current template.
+		Extensions: []string{".tmpl", ".html"},          // Specify extensions to load for templates.
+		Funcs:      []template.FuncMap{unescapeFuncMap}, // Specify helper function maps for templates to access.
+		Charset:    "UTF-8",                             // Sets encoding for json and html content-types. Default is "UTF-8".
+		IndentJSON: true,                                // Output human readable JSON
+	}))
+
+	staticOptions := martini.StaticOptions{Prefix: "assets"}
+	m.Use(martini.Static("assets", staticOptions))
+
+	m.Get("/", indexHandler)
+	m.Get("/write", writeHandler)
+	m.Get("/edit/:id", editHandler)
+	m.Get("/delete/:id", deleteHandler)
+	m.Post("/SavePost", savePostHandler)
+	m.Post("/gethtml", getHtmlHandler)
+
+	m.Run()
+
+}
